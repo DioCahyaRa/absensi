8b37e89083c6a071b24fbe33d37c9b41a206fb1a
DioCahyaRa <diocahyaramadhan21@gmail.com>
2021-06-23 11:53:07 +0700

    add function costruct and signup

--- application/controllers/Login_c.php
+++ application/controllers/Login_c.php
@@ -3,7 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login_c extends CI_Controller {
 
+    public function __construct(){
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index(){
         $this->load->view('Login_v');
+    }
+
+    public function SignUp(){
+        $this->load->view('Signup_v');
     }
 }
