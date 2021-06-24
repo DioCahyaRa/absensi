22165b907e45dc5a4076bae21b848454805b68d3
DioCahyaRa <diocahyaramadhan21@gmail.com>
2021-06-24 18:49:06 +0700

    add link cdn datatables

--- application/views/User/Footer_v.php
+++ application/views/User/Footer_v.php
@@ -26,5 +26,26 @@
         </div>
         <script src="<?= base_url('Assets/modular-admin/dist/')?>js/vendor.js"></script>
         <script src="<?= base_url('Assets/modular-admin/dist/')?>js/app.js"></script>
+
+        <!-- DataTables -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.6.0/dt-1.10.25/b-1.7.1/b-html5-1.7.1/b-print-1.7.1/date-1.1.0/fh-3.1.9/r-2.2.9/sp-1.3.0/sl-1.3.3/datatables.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+    
+
+        <script>
+            $(document).ready(function() {
+                $('#example').DataTable();
+            } );
+
+            // $('#myTable').DataTable( {
+            //     buttons: [
+            //         'copy', 'excel', 'pdf'
+            //     ]
+            // } );
+        </script>
+
+        <!-- Tutup DataTables -->
     </body>
 </html>
