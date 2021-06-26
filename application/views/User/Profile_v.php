47fb250d4311cf0abbe6f2fec7fffc8a4750946d
DioSkuy <dioskuy@DioSkuys-MacBook-Pro.local>
2021-06-26 19:01:07 +0700

    change view profile text

--- application/views/User/Profile_v.php
+++ application/views/User/Profile_v.php
@@ -13,10 +13,63 @@
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body" style="padding:50px;">
-                                        <p class="card-text">Nama</p>
-                                        <p class="card-text">Nomor Pegawai</p>
-                                        <p class="card-text">Email</p>
-                                        <p class="card-text">Jabatan</p>
+                                    <?php foreach($user as $u):?>
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h6 class="mb-0">Full Name</h6>
+                                            </div>
+                                            <div class="col-sm-6 text-secondary">
+                                                : <?= $u['nama']?>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h6 class="mb-0">NIT</h6>
+                                            </div>
+                                            <div class="col-sm-6 text-secondary">
+                                                : <?= $u['nit']?>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h6 class="mb-0">Tanggal Lahir</h6>
+                                            </div>
+                                            <div class="col-sm-6 text-secondary">
+                                                : <?= $u['tgl_lahir']?>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h6 class="mb-0">Jabatan</h6>
+                                            </div>
+                                            <div class="col-sm-6 text-secondary">
+                                                : <?= $u['jabatan']?>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h6 class="mb-0">Email</h6>
+                                            </div>
+                                            <div class="col-sm-6 text-secondary">
+                                                : <?= $u['email']?>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <h6 class="mb-0">Tanggal Masuk</h6>
+                                            </div>
+                                            <div class="col-sm-6 text-secondary">
+                                                : <?= $u['tgl_masuk']?>
+                                            </div>
+                                        </div>
+                                    <?php endforeach;?>
+                                        <br>
+                                    <button class="btn btn-success btn-oval"><i class="fa fa-pencil"></i> EDIT</button>
                                     </div>
                                 </div>
                             </div>
