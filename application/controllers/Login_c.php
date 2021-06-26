1b3f8d68bd1375553a29d691e27a4f699cf3a5ad
DioSkuy <dioskuy@DioSkuys-MacBook-Pro.local>
2021-06-26 13:35:01 +0700

    new updated

--- application/controllers/Login_c.php
+++ application/controllers/Login_c.php
@@ -16,6 +16,45 @@ class Login_c extends CI_Controller {
         $this->load->view('Signup_v');
     }
 
+    public function Act_Login(){
+
+        $this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[6]',[
+            'min_length[6]' => 'Password too Short Min 6 character'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('Login_v');
+        }else{
+            $nit = $this->input->post('nit');
+            $password = $this->input->post('password');
+            $user = $this->db->get_where('user', ['nit'=>$nit])->row_array();
+        if($user){
+            if(password_verify($password,$user['password'])){
+                $data = [
+                    'nit' => $nit,
+                    'nama' => $user['nama'],
+                    'role' => $user['admin'],
+                    'isLogged' => TRUE
+                ];
+                $this->session->set_userdata($data);
+                redirect('User/Dashboard_c');
+            }else{
+                // Email Password Salah
+                $this->session->set_flashdata('msg','<script>Swal.fire("","sorry your username or password is wrong","error")</script>');
+                
+                redirect(base_url().'Login_c');
+            }
+        }else{
+            // User Email Belum Terdaftar
+            $this->session->set_flashdata('msg','<script>Swal.fire("","sorry your username not regis","error")</script>');
+            redirect('Login_c');
+        }
+
+            
+        }
+
+    }
+
     public function SignUpAct(){
         $this->form_validation->set_rules('nit', 'Nit', 'trim|is_unique[user.nit]');
         $this->form_validation->set_rules('email', 'Email', 'trim|is_unique[user.email]');
@@ -33,7 +72,6 @@ class Login_c extends CI_Controller {
                 'jabatan' => $this->input->post('jabatan'),
                 'email' => $this->input->post('email'),
                 'password' => password_hash($this->input->post('password'),PASSWORD_DEFAULT),
-                'tgl_masuk' => $this->input->post('tgl_masuk'),
                 'status' => 'active',
                 'role' => 'admin'
             ];
