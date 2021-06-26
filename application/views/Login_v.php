72580175b9a2ec2426bb729c954dc6c67b4eae5e
DioSkuy <dioskuy@DioSkuys-MacBook-Pro.local>
2021-06-26 12:42:24 +0700

    add for action and form error

--- application/views/Login_v.php
+++ application/views/Login_v.php
@@ -26,11 +26,7 @@
         ?>
 	<section class="ftco-section">
 		<div class="container">
-			<!-- <div class="row justify-content-center">
-				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">LOGIN</h2>
-				</div>
-			</div> -->
+
 			<div class="row justify-content-center">
 				<div class="col-md-6 col-lg-4">
 					<div class="login-wrap py-5">
@@ -46,11 +42,7 @@
 	            	<div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-lock"></span></div>
 	              <input type="password" class="form-control" placeholder="Password" required>
 	            </div>
-	            <div class="form-group d-md-flex">
-								<!-- <div class="w-100 text-md-right">
-									<a href="#">Forgot Password</a>
-								</div> -->
-	            </div>
+
 	            <div class="form-group">
 	            	<button type="submit" class="btn form-control btn-primary rounded submit px-3">Login</button>
 	            </div>
