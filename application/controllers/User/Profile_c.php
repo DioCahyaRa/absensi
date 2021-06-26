ccdac31507b9b9567727c8878835335b3f4981ec
DioSkuy <dioskuy@DioSkuys-MacBook-Pro.local>
2021-06-26 19:03:40 +0700

    connect User_m for function User_profile()

--- application/controllers/User/Profile_c.php
+++ application/controllers/User/Profile_c.php
@@ -6,10 +6,12 @@ class Profile_c extends CI_Controller {
     public function __construct(){
         parent::__construct();
         $this->load->library('form_validation');
+        $this->load->model('User_m');
     }
 
     public function index(){
         $data['title'] = 'Profile';
+        $data['user'] = $this->User_m->User_profile();
         $this->load->view('User/Head_v',$data);
         $this->load->view('User/Profile_v');
         $this->load->view('User/Footer_v');
