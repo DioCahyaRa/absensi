6c688d6231f2f72b0bff2052d76f5256434d423d
DioSkuy <dioskuy@DioSkuys-MacBook-Pro.local>
2021-06-27 21:01:46 +0700

    add function edit profile User

--- application/controllers/User/Profile_c.php
+++ application/controllers/User/Profile_c.php
@@ -10,14 +10,24 @@ class Profile_c extends MY_Controller {
     }
 
     public function index(){
-        if($_SESSION){
-            redirect('User/Dashboard_c');
-        }else{
-            $data['title'] = 'Profile';
-            $data['user'] = $this->User_m->User_profile();
-            $this->load->view('User/Head_v',$data);
-            $this->load->view('User/Profile_v');
-            $this->load->view('User/Footer_v');
-        }
+        $data['title'] = 'Profile';
+        $data['user'] = $this->User_m->User_profile();
+        $this->load->view('User/Head_v',$data);
+        $this->load->view('User/Profile_v');
+        $this->load->view('User/Footer_v');
+    }
+
+    public function edit_profile(){
+        $data_update = [
+            'nama' => $this->input->post('nama'),
+            'tgl_lahir' => $this->input->post('tgl_lahir'),
+            'email' => $this->input->post('email')
+        ];
+        $nit = $this->input->post('nit');
+
+        $this->db->where('nit', $nit);
+        $this->db->update('user', $data_update);
+        $this->session->set_flashdata('msg-update','<script>Swal.fire("","Successfull Update Profile","success")</script>');
+        redirect('User/Profile_c');
     }
 }
