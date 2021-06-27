6ebc903dc34b1706af47077516d61c63b71208cc
DioSkuy <dioskuy@DioSkuys-MacBook-Pro.local>
2021-06-27 16:37:53 +0700

    add function absensi per user

--- application/models/User_m.php
+++ application/models/User_m.php
@@ -4,5 +4,12 @@
         public function User_profile(){
             return $this->db->get_where('user',['nit'=>$_SESSION['nit']])->result_array();
         }
+
+        public function Absensi_user(){
+            $this->db->select('*');
+            $this->db->from('absensi');
+            $this->db->where('nit',$_SESSION['nit']);
+            return $this->db->get()->result_array();
+        }
     }
 ?>
